ee7eca164bbddc3e13af3655260de9ed9e3cddb7
yavilarobledo <143647435+yavilarobledo@users.noreply.github.com>
2023-12-13 23:43:03 -0500

    Updated  MyJobs.js to have a table when approving a fixer applicant

--- src/Pages/MyJobs.js
+++ src/Pages/MyJobs.js
@@ -43,54 +43,77 @@ const MyJobs = () => {
             <br /> <br />
               {user
         ? <h2 className="text-center">My Jobs</h2>
-        : <h2  className="text-center">You must be logged in to view this page.
+        :   <h2  className="text-center">You must be logged in to view this page.
             <br />
-            <a href="/login"><button className="btn bg-black btn-dark mt-3" >Log In</button></a></h2>}
+            <a href="/login"><button className="btn bg-black btn-dark mt-3" >Log In</button></a>
+            </h2>
+            }
+
+
             {jobs.length ? (
                 <div className="card-group">
-                    {jobs.map (job => (
+                    <div className="row">
+                        {jobs.map (job => (
+                            <div className="col-lg">
 
-                                    <div className="card text-center shadow m-2" key={job.UserId} >
+                                <div className="card text-center shadow m-3" key={job.UserId} >
 
-                                    <div className="card-body text-dark">
+                                        <div className="card-header">
+                                                Date Needed: {moment(job.DateNeeded).format('MMM DD, YYYY')}
+                                        
+                                        </div>
 
 
-                                            <h4 className="card-title">{job.ServiceRequest}</h4>
 
-                                            <p className="card-text text-secondary">
-                                                {job.Desc}
-                                            </p>
+                                        <div className="card-body text-dark">
 
-                                            <p className="card-text text-secondary">
-                                           {moment(job.DateNeeded).format('MMM DD, YYYY')}
+                                                    <h5 className="card-title">{job.ServiceRequest}</h5>
 
-                                            </p>
-                                            <p className="card-text text-secondary">
-                                            {job.City}
-                                            </p>
-                                            {job.Accepted ? (
-                                            <p>Fixer{job.ApprovedApplicant.substring(0,5)}<span style={{display:"inline-block",padding:5,background:"limegreen",color:"white"}}>Approved</span></p>
-                                            ) : (
-                                                <ul style={{listStyle:"none",paddingLeft:0}}>
-                                                    {job.Applicants.map(applicant=>(
-                                                        <li><a>Fixer{applicant.substring(0,5)}</a><button onClick={() => approveApplicant(job._id,applicant)}>Approve</button></li>
-                                                    ))}
-                                                </ul>
-                                            )}
+                                                    <p className="card-text text-secondary">
+                                                        {job.Desc}
+                                                    </p>
+                            
+                                                    {job.Accepted ? (
+                                                    <p>Fixer{job.ApprovedApplicant.substring(0,5)}<span style={{display:"inline-block",padding:5,background:"limegreen",color:"white"}}>Approved</span></p>
+                                                    ) : (
+                                                        <ul style={{listStyle:"none",paddingLeft:0}}>
+                                                            {job.Applicants.map(applicant=>(
+                                                                <>
+                                                                <table class="table">
+                                                                <thead>
+                                                                  <tr>
+                                                                    <th colspan="2">Fixer Applicants</th>
+                                                                  </tr>
+                                                                </thead>
+                                                                <tbody>
+                                                                  <tr>
+                                                                    <th scope="row">{applicant.substring(0,5)}</th>
+                                                                    <td><button onClick={() => approveApplicant(job._id,applicant)}> Approve</button></td>
+                                                                  </tr>
+                                                                </tbody>
+                                                              </table>
+                                                              </>
+                                                              
+                                                                // <li><a>Fixer {applicant.substring(0,5)}</a> <button onClick={() => approveApplicant(job._id,applicant)}> Approve</button></li>
+                                                            ))}
+                                                        </ul>
+                                                    )}
+                                                
 
-                                    </div>
-                                    <div class="card-footer bg-transparent border-success">
-                                    <button>Apply</button>
-                                    </div>
-                                    </div>
-                    ))}
+                                        </div>
+                                        
+                                </div> 
+                            </div>   
+                        ))}
+                    </div>
                 </div>
             ):(
-                <h2></h2>  
+                <h2></h2>
 
             )}
         </>
-     );
+    );
 }
  
 export default MyJobs;
+
